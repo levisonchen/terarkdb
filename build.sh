89991889d49741bfc9c04466ff1f8f1c567dc3e6
Changlong Chen <chenchanglong@bytedance.com>
2021-09-27 16:23:58 +0800

    Change develop envonriment setting.

--- build.sh
+++ build.sh
@@ -16,18 +16,20 @@ else
     echo you must ensure libaio-dev have been installed
 fi
 
-git submodule update --init --recursive
+#git submodule update --init --recursive
 
 if [ "$WITH_TESTS" == "1" ]; then
   WITH_TESTS=ON
   echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
   echo "You are building TerarkDB with tests, so debug mode is enabled"
-  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
-  cd $BASE/$OUTPUT && make -j $(nproc) && make install
+  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Debug -DWITH_TESTS=${WITH_TESTS} -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON -DWITH_ZENFS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  cd $BASE/$OUTPUT && make -j $(nproc)
   echo "You are building TerarkDB with tests, so debug mode is enabled"
 else
   WITH_TESTS=OFF
   echo "build $BUILD_TYPE, with_tests = $WITH_TESTS"
-  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Release -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON
-  cd $BASE/$OUTPUT && make -j $(nproc) && make install
+  cd $BASE/$OUTPUT && cmake ../ -DCMAKE_INSTALL_PREFIX=$OUTPUT -DCMAKE_BUILD_TYPE=Release -DWITH_TOOLS=ON -DWITH_TERARK_ZIP=ON -DWITH_ZENFS=ON -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
+  cd $BASE/$OUTPUT && make -j $(nproc)
 fi
+rm -f $BASE/compile_commands.json
+ln -s $BASE/$OUTPUT/compile_commands.json $BASE/
